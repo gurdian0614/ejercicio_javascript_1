f7024716b8f66ee3d6a4d1968d874f9e0ce16d22
gurdian0614 <ivan.gurdian@outlook.com>
2024-07-23 20:28:22 -0600

    Agregando validaciones, funcion limpiar camos

--- js/index.js
+++ js/index.js
@@ -1,21 +1,49 @@
 function calcular() {
-    let a = parseInt(document.getElementById("valorA").value)
-    let b = parseInt(document.getElementById("valorB").value)
-    let c = parseInt(document.getElementById("valorC").value)
-    
-    if (a === 0) {
-        alert("No se puede dividir entre cero")
+    let strA = document.getElementById("valorA").value
+    let strB = document.getElementById("valorB").value
+    let strC = document.getElementById("valorC").value
+
+    if (strA === "") {
+        warning("Valor a, en blanco")
+    } else if (strB === "") {
+        warning("Valor b, en blanco")
+    } else if (strC === "") {
+        warning("Valor c, en blanco")
     } else {
-        let discriminante = b * b - 4 * a * c;
-
-        if (discriminante < 0) {
-            alert("La raíz cuadrada no puede ser negativa")
+        let a = parseInt(strA)
+        let b = parseInt(strB)
+        let c = parseInt(strC)
+        
+        if (a === 0) {
+            warning("No se puede dividir entre cero")
         } else {
-            let x1 = (-b + Math.sqrt(discriminante)) / (2 * a)
-            let x2 = (-b - Math.sqrt(discriminante)) / (2 * a)
-
-            document.getElementById("valorResultadoX1").value = x1
-            document.getElementById("valorResultadoX2").value = x2
+            let discriminante = b * b - 4 * a * c;
+    
+            if (discriminante < 0) {
+                warning("La raíz cuadrada no puede ser negativa")
+            } else {
+                let x1 = (-b + Math.sqrt(discriminante)) / (2 * a)
+                let x2 = (-b - Math.sqrt(discriminante)) / (2 * a)
+    
+                document.getElementById("valorResultadoX1").value = x1
+                document.getElementById("valorResultadoX2").value = x2
+            }
         }
     }
+}
+
+function warning(mensaje) {
+    Swal.fire({
+        icon: "warning",
+        title: "Oops...",
+        text: mensaje
+    });
+}
+
+function limpiar() {
+    document.getElementById("valorA").value = ""
+    document.getElementById("valorB").value = ""
+    document.getElementById("valorC").value = ""
+    document.getElementById("valorResultadoX1").value = ""
+    document.getElementById("valorResultadoX2").value = ""
 }
